cc45db7a4c7d6ce70329b173d084f94e7fb42675
wzk <wzk1335@163.com>
2026-09-08 12:55:23 +0800

    Release version 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.quzhi.lite"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
 
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a")
